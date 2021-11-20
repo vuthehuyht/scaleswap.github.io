80b11cbc9340ddace1800747f263c1644fbb5b3e
Vu The Huy <vuthehuy.hus@gmail.com>
2021-11-20 22:50:32 +0700

    deploy 3 new webs

---
+++ pintu/_next/static/chunks/104.3383594e357622549bde.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[104,40,115],{SVOo:function(n,t,c){"use strict";c.r(t);var s=c("nKUr"),u=(c("q1tI"),c("E45T"));t.default=function(){return Object(s.jsx)(u.default,{})}},hDsp:function(n,t,c){"use strict";var s=c("YFqc"),u=c.n(s);t.a=u.a}}]);
+//# sourceMappingURL=104.3383594e357622549bde.js.map
